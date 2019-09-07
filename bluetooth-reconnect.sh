2fbd916df18e6c9ada5f13f0747e2b3398d7b3b6
Clay Miller <clay@smockle.com>
2019-09-07 09:31:40 -0400

    Update 'bluetooth-reconnect.sh'

--- bluetooth-reconnect.sh
+++ bluetooth-reconnect.sh
@@ -1,10 +1,27 @@
 #!/usr/bin/env zsh
 
 bluetooth-reconnect() {
-  local MAC_ADDRESS=$1
+  let "ITERATIONS = ${1} - 1"
   shift 1
-  if [ bluetoothctl info "${MAC_ADDRESS}" | grep -Fq 'Connected: no' ]; then
-    echo -e "disconnect\nconnect ${MAC_ADDRESS}\nexit" | bluetoothctl
+  local MAC_ADDRESS="${1}"
+  shift 1
+  if [ "${ITERATIONS}" -eq "0" ]; then
+    echo "Failed to connect"
+    exit 1
+  fi
+  if ! bluetoothctl info "${MAC_ADDRESS}" &>/dev/null; then
+    echo "Invalid address"
+    exit 1
+  fi 
+  if $(bluetoothctl info "${MAC_ADDRESS}" | grep -Fq 'Connected: no'); then
+    echo "Connecting (iterations: ${ITERATIONS})"
+    echo -e "disconnect\nconnect ${MAC_ADDRESS}\nexit" | bluetoothctl &>/dev/null
+    sleep 1
+    bluetooth-reconnect "${ITERATIONS}" "${MAC_ADDRESS}"
+  else
+    echo "Connected"
   fi
   unset MAC_ADDRESS
 }
+
+bluetooth-reconnect 6 "${@}"
